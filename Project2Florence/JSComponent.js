e1a96f598156d1a3a80bf9c8f3de02c9b645dcf3
Fchapell11 <80227584+Fchapell11@users.noreply.github.com>
2021-03-07 18:28:47 -0500

    Add files via upload

---
+++ Project2Florence/JSComponent.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+
+var tag = document.getElementById('demo');
+var button = document.querySelector('button');
+button.addEventListener('click', function(){
+tag.classList.toggle('demo');
+});
